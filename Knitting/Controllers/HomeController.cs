406dc5878851cf94f6c1e9ed4703934fcdf3c7b5
katehendrickson1 <119885479+katehendrickson1@users.noreply.github.com>
2024-02-07 14:51:26 -0700

    Add project files.

---
+++ Knitting/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+using Knitting.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Knitting.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Calculator()
+        {
+            // write code here
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Knitting/wwwroot/js/site.js
@@ -0,0 +1,34 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+/* function calculateCost() 
+{
+    let numHours = parseFloat(document.getElementById("numHours").value)
+
+    if (numHours >= 0 && !isNaN(numHours)) {
+        let totalCost = 0.25 * numHours
+
+        document.getElementById("displayCost").innerHTML = "Total cost: $" + totalCost.toFixed(2);
+    }
+    else
+    {
+        alert("Please enter a valid, positive number.")
+    }
+} */
+
+//jQuery conversion of the javaScript function. 
+$(document).ready(function () {
+    $("#calculateButton").click(function () {
+        let numHours = parseFloat($("#numHours").val());
+
+        //check if the numHours inputted by the user is a valid positive number & calculate
+        if (numHours >= 0 && !isNaN(numHours)) {
+            let totalCost = 0.25 * numHours;
+            $("#displayCost").html("Total cost: $" + totalCost.toFixed(2));
+        } else {
+            alert("Please enter a valid, positive number.");
+        }
+    });
+});
